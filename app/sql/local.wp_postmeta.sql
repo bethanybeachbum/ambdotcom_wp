696602a768bd121f7d24127740710709c5781cf4
Mark Brown <allenmarkbrown@gmail.com>
2019-07-09 17:38:28 -0400

    notes for initial commit

---
+++ app/sql/local.wp_postmeta.sql
@@ -0,0 +1,23 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_postmeta` VALUES
+(1,2,"_wp_page_template","default"),
+(2,3,"_wp_page_template","default"),
+(3,5,"_edit_lock","1561465338:1"),
+(6,1,"_wp_trash_meta_status","publish"),
+(7,1,"_wp_trash_meta_time","1561465488"),
+(8,1,"_wp_desired_post_slug","hello-world"),
+(9,1,"_wp_trash_meta_comments_status","a:1:{i:1;s:1:\"1\";}"),
+(10,8,"_edit_lock","1561465884:1"),
+(13,10,"_edit_lock","1561490777:1"),
+(14,10,"_wp_trash_meta_status","publish"),
+(15,10,"_wp_trash_meta_time","1561490931"),
+(16,10,"_wp_desired_post_slug","sample-page-2"),
+(17,2,"_edit_lock","1562499205:1"),
+(18,13,"_edit_lock","1562498928:1"),
+(19,15,"_edit_lock","1561729315:1"),
+(20,3,"_edit_lock","1561812797:1"),
+(21,17,"_edit_lock","1562499239:1"),
+(22,19,"_edit_lock","1562590113:1"),
+(23,22,"_edit_lock","1561817458:1");
---
+++ app/sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"markbrown","$P$BBUiahPvG4YOXMx7Icie/dHJtyxM1m/","markbrown","dev-email@flywheel.local","","2019-06-25 01:47:43","",0,"markbrown");
