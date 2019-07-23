0cd632eb6d179f86287735b90004db787cd0fb07
Mark Brown <allenmarkbrown@gmail.com>
2019-07-23 08:16:07 -0400

    adding classes up to #25

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -2,7 +2,7 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_usermeta` VALUES
-(1,1,"nickname","markbrown"),
+(1,1,"nickname","Mark"),
 (2,1,"first_name",""),
 (3,1,"last_name",""),
 (4,1,"description",""),
@@ -17,6 +17,9 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers",""),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"e5087dde1a9c951800360b970ef2c57640e56f5fd17e697dd0ceb2b11ba5ae48\";a:4:{s:10:\"expiration\";i:1562671847;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36\";s:5:\"login\";i:1562499047;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","4"),
-(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}");
+(16,1,"session_tokens","a:1:{s:64:\"e8fa710f5c8b901dbefc7b0566f166bc1761b30623395cadfc1fac50f24ec090\";a:4:{s:10:\"expiration\";i:1563364290;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36\";s:5:\"login\";i:1563191490;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","59"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"nav_menu_recently_edited","3");
--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -2,4 +2,4 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
-(1,"markbrown","$P$BBUiahPvG4YOXMx7Icie/dHJtyxM1m/","markbrown","dev-email@flywheel.local","","2019-06-25 01:47:43","",0,"markbrown");
+(1,"markbrown","$P$BBUiahPvG4YOXMx7Icie/dHJtyxM1m/","markbrown","dev-email@flywheel.local","","2019-06-25 01:47:43","",0,"Mark");
